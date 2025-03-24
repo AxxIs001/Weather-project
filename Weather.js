8d2da0e1a8f325009acf3d8d7cf1f4bdf6ed4dcf
Chyngyz Abdikerimov <411021385@gms.ndhu.edu.tw>
2025-03-25 00:23:39 +0800

    update

--- Weather.js
+++ Weather.js
@@ -1,5 +1,5 @@
 const apiKey = "94a252cd2236c1ad47bf02023af36485";
- const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
  
  const searchBox = document.querySelector(".search input");
  const searchBtn = document.querySelector(".search button");
